475c6493fbe41c96c35c3f4ffd269d32e157135e
Jisun696 <istiakkashem35@gmail.com>
2024-01-27 00:45:47 +0600

    adding dark-light mode done

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,2 +1,24 @@
-const themeToggleBtns = document.querySelectorAll('#theme-toggle');
-themeToggleBtns;
+const darkMode = () => {
+  const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+
+  // state
+  const theme = localStorage.getItem('theme');
+
+  // On mount
+  theme && document.body.classList.add(theme); // theme = 'light-mode'
+
+  //Handlers
+  const handleThemeToggle = () => {
+    document.body.classList.toggle('light-mode');
+    if (document.body.classList.contains('light-mode')) {
+      localStorage.setItem('theme', 'light-mode');
+    } else {
+      localStorage.removeItem('theme');
+      document.body.removeAttribute('class');
+    }
+  };
+
+  themeToggleBtns.forEach((btn) => btn.addEventListener('click', handleThemeToggle));
+};
+
+export default darkMode;
