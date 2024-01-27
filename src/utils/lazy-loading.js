b6ea72e7c079c5d036ec1683b553d6860b57fa8d
Jisun696 <istiakkashem35@gmail.com>
2024-01-28 03:22:17 +0600

    removed the placeholder images from html img tags and used the loading attribute with the value 'lazy'

--- src/utils/lazy-loading.js
+++ src/utils/lazy-loading.js
@@ -7,7 +7,7 @@ const lazyLoading = () => {
       if (entry.isIntersecting) {
         // console.log(entry.target);
         let img = entry.target;
-        img.src = img.dataset.src;
+        // img.src = img.dataset.src;  //previously used placeholder image as src in html img tag & the original src as data-src
         img.classList.remove('loading');
         img.classList.add('loaded');
         observer.unobserve(img);
